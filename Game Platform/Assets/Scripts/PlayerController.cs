4d69d216fda9596f3bc29cc5729a45431f6628b8
alesanti99 <126773679+alesanti99@users.noreply.github.com>
2023-07-11 12:21:30 +0200

    first commit

---
+++ Game Platform/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 8f;
+    private float direction = 0f;
+    private Rigidbody2D player;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        direction = Input.GetAxis("Horizontal");
+
+        if (direction > 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else if (direction < 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision) { 
+
+        if (collision.gameObject.CompareTag("Win"))
+        {
+            SceneManager.LoadScene("Success");
+        }
+        if (collision.gameObject.CompareTag("Danger"))
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+        if (collision.gameObject.CompareTag("Score"))
+        {
+            Scoring.totalScore += 1;
+             collision.gameObject.SetActive(false);
+        }
+    }
+}
